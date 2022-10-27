e73b2b070c8af36fe680d1015a1d7e6b98109a3e
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-10-27 10:43:43 -0500

    cont,mod,schem

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,39 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SearchMovies from './pages/SearchMovies';
+import SavedMovies from './pages/SavedMovies';
+import './App.scss';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
+
 function App() {
   return (
-    <div className="">
-      <h1>Hello World</h1>
-    </div>
+    <ApolloProvider client={client}>
+        <Router>
+          <>
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={SearchMovies} />
+              <Route exact path='/saved' component={SavedMovies} />
+              <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+            </Switch>
+          </>
+        </Router>
+    </ApolloProvider>
   );
 }
 
